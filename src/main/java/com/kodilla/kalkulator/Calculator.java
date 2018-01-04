251b73145c3fd6b87cab2b3cfb84de7e5844b626
smichon <sylwester.michon@gmail.com>
2018-01-04 23:17:32 +0100

    Changes in display

--- src/main/java/com/kodilla/kalkulator/Calculator.java
+++ src/main/java/com/kodilla/kalkulator/Calculator.java
@@ -3,6 +3,8 @@ package com.kodilla.kalkulator;
 public class Calculator {
     private double varA;
     private double varB;
+    private double resultSum;
+    private double resultDiff;
 
     public double getVarA() {
         return varA;
@@ -12,21 +14,31 @@ public class Calculator {
         return varB;
     }
 
-    public double addAToB(double varA, double varB) {
-        return varA + varB;
+    public double getResultSum() {
+        return resultSum;
+    }
 
+    public double getResultDiff() {
+        return resultDiff;
+    }
+
+    public double addAToB(double varA, double varB) {
+        resultSum=varA+varB;
+        System.out.println("Sum of "+varA+" and "+varB+" is "+getResultSum());
+        return getResultSum();
     }
 
     public double subtractBFromA(double varA, double varB) {
+        resultDiff=varA-varB;
+        System.out.println(varB+" subtracted from  "+varA+" is "+ getResultDiff());
         return varA - varB;
     }
     public static void main(String[] args){
         Calculator calculator = new Calculator();
+        System.out.println("That is a simple calculator, which can add and subtract 2 numbers\n---------");
 
-        double sum = calculator.addAToB(20.0,10.0);
-        double diff = calculator.subtractBFromA(10.0,5.0);
+        calculator.addAToB(20.0, 10.0);
+        calculator.subtractBFromA(10.0,5.0);
 
-        System.out.println("Sum is:"+sum);
-        System.out.println("Diff is: "+diff);
     }
 }
